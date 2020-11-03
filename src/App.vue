04bb9953c6d5d466d8973152fa54c173a37de840
Stephane Dubedat <steph.dubedat@gmail.com>
2020-11-03 18:21:47 +0000

    add reproducer

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <Wtf msg="Welcome to Your Vue.js + TypeScript App" />
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import Wtf from "./components/Wtf.vue";
+
+@Options({
+  components: {
+    Wtf
+  }
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
